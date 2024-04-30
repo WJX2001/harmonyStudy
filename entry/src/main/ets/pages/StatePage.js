b1f52af70d2820b6a492b308fe760c0d854a7a13
WJX2001 <1758807220@qq.com>
2024-04-30 09:39:54 +0800

    first commit

---
+++ entry/src/main/ets/pages/StatePage.js
@@ -0,0 +1,23 @@
+"use strict";
+// 复杂类型
+class Person {
+    constructor(name, age, gf) {
+        this.name = name;
+        this.age = age;
+        this.gf = gf;
+    }
+}
+struct StatePage extends   {
+    constructor(// 复杂类型
+    // 复杂类型
+    // 复杂类型
+    ) {
+        // 复杂类型
+    }
+    build() {
+            .width('100%')
+            .height('100%')
+            .justifyContent(FlexAlign.Center);
+    }
+}
+//# sourceMappingURL=StatePage.js.map
