44eb4af8f7eddf1495086259847e9d4411dca4d5
WJX2001 <1758807220@qq.com>
2024-05-02 11:36:11 +0800

    feat: Ability跳转

--- entry/src/main/ets/documentability/DocumentAbility.ts
+++ entry/src/main/ets/documentability/DocumentAbility.ts
@@ -2,7 +2,7 @@ import UIAbility from '@ohos.app.ability.UIAbility';
 import hilog from '@ohos.hilog';
 import window from '@ohos.window';
 
-export default class EntryAbility extends UIAbility {
+export default class DocumentAbility extends UIAbility {
   onCreate(want, launchParam) {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
   }
@@ -15,7 +15,7 @@ export default class EntryAbility extends UIAbility {
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
 
-    windowStage.loadContent('pages/Index', (err, data) => {
+    windowStage.loadContent('pages/DocumentPage', (err, data) => {
       if (err.code) {
         hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
         return;
@@ -31,14 +31,11 @@ export default class EntryAbility extends UIAbility {
 
   onForeground() {
     // Ability has brought to foreground
-    // 申请系统需要的资源，或者重新在onBackground中释放的资源
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onForeground');
   }
 
   onBackground() {
     // Ability has back to background
-    // 释放UI界面不可见时无用的资源，或者在此回调中执行较为耗时的操作
-    // 例如状态保存等
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onBackground');
   }
 }
